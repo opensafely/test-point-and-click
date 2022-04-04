03bedd641b115e67c3fb76ec5f362d170296dd7d
Lucy Bridges <lucy.bridges@thedatalab.org>
2022-04-04 16:05:36 +0100

    Calculate code counts by adding a variable for each
    This creates a very wide dataset, by adding a variable for each code in
    a codelist and counting how many times it's used for each patient.

--- analysis/study_definition_counts.py
+++ analysis/study_definition_counts.py
@@ -5,7 +5,7 @@ from cohortextractor import (
     patients,
 )
 
-from analysis.study_utils import last_day_of_month
+from analysis.study_utils import calculate_code_frequency, last_day_of_month
 from variables import selected_codelist, start_date, end_date
 
 
@@ -20,21 +20,5 @@ study = StudyDefinition(
     },
     index_date=index_date,
     population=patients.all(),
-    # calculate how often codes are used
-    codes=patients.with_these_clinical_events(
-        codelist=selected_codelist,
-        between=["index_date", latest_date],
-        returning="code",
-        return_expectations={"category": {
-            "ratios": {str(271649006): 0.6, str(314439003): 0.2, str(314440001): 0.2}},
-        },
-    ),
-    matches=patients.with_these_clinical_events(
-        codelist=selected_codelist,
-        between=["index_date", latest_date],
-        returning="number_of_matches_in_period",
-        return_expectations={
-                "int": {"distribution": "normal", "mean": 2, "stddev": 0.5}
-        },
-    ),
+    **calculate_code_frequency(start_date, end_date, selected_codelist)
 )
--- analysis/study_utils.py
+++ analysis/study_utils.py
@@ -1,7 +1,7 @@
 from datetime import date
 from dateutil import rrule, relativedelta
 
-from cohortextractor import patients
+from cohortextractor import patients, codelist
 
 
 def last_day_of_month(dt):
@@ -24,3 +24,21 @@ def calculate_months(start_date, end_date, selected_codelist):
         )
 
     return months
+
+
+def calculate_code_frequency(start_date, end_date, selected_codes):
+    start_date_formatted = date.strftime(start_date, "%Y-%m-%d")
+    end_date_formatted = date.strftime(end_date, "%Y-%m-%d")
+
+    variables = {}
+    for code in selected_codes:
+        variables[f"snomed_{code}"] = patients.with_these_clinical_events(
+                codelist([code], system="snomed"),
+                between=[start_date_formatted, end_date_formatted],
+                returning="number_of_matches_in_period",
+                return_expectations={
+                    "incidence": 0.1,
+                    "int": {"distribution": "normal", "mean": 3, "stddev": 1},
+                }
+        )
+    return variables
