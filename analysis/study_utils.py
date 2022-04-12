f9d62506a17dbc29f4c0db5080bae7adcb53fc5f
Louis Fisher <37254732+LFISHER7@users.noreply.github.com>
2022-04-12 12:41:07 +0100

    Add suppression function Adds a function that redacts all rows with low counts and groups the redacted counts into a new row with the code "Other"

--- analysis/study_utils.py
+++ analysis/study_utils.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from dateutil import relativedelta
 
 
@@ -7,3 +8,44 @@ def last_day_of_month(dt):
 
 def last_day_of_week(dt):
     return dt + relativedelta.relativedelta(day=7)
+
+
+def group_low_values(df, count_column, code_column, threshold):
+    """Suppresses low values and groups suppressed values into
+    a new row "Other".
+
+    Args:
+        df: A measure table of counts by code.
+        count_column: The name of the count column in the measure table.
+        code_column: The name of the code column in the codelist table.
+        threshold: Redaction threshold to use
+    Returns:
+        A table with redacted counts
+    """
+
+    # get sum of any values <= threshold
+    suppressed_count = df.loc[df[count_column] <= threshold, count_column].sum()
+
+    # if no values are suppressed we don't need to do anything
+    if suppressed_count == 0:
+        pass
+
+    # if suppressed values >0 ensure total suppressed count > threshold
+    else:
+        # redact counts <= threshold
+        df.loc[df[count_column] <= threshold, count_column] = np.nan
+
+        # if suppressed count <= threshold redact further values
+        while suppressed_count <= threshold:
+            suppressed_count += df[count_column].min()
+            df.loc[df[count_column].idxmin(), :] = np.nan
+
+        # drop all rows where count column is null
+        df = df.loc[df[count_column].notnull(), :]
+
+        # add suppressed count as "Other" row (if > threshold)
+        if suppressed_count > threshold:
+            suppressed_count = {"code": "Other", "count": suppressed_count}
+            df = df.append(suppressed_count, ignore_index=True)
+
+    return df
