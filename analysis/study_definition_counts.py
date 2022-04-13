549884cceb146ddc5738c227dd4725db43f9a558
HelenCEBM <helencebm@gmail.com>
2022-04-13 17:07:35 +0100

    Only do counts for whole period

--- analysis/study_definition_counts.py
+++ analysis/study_definition_counts.py
@@ -1,3 +1,5 @@
+from datetime import date, datetime
+
 from cohortextractor import (
     StudyDefinition,
     codelist,
@@ -5,15 +7,18 @@ from cohortextractor import (
     patients,
 )
 
-from variables import codelist_file
+from variables import codelist_file, study_start_date, study_end_date
 
 
-def calculate_code_frequency(selected_codes):
+def calculate_code_frequency(start_date, end_date, selected_codes):
+    start_date_formatted = date.strftime(start_date, "%Y-%m-%d")
+    end_date_formatted = date.strftime(end_date, "%Y-%m-%d")
+
     variables = {}
     for code in selected_codes:
         variables[f"snomed_{code}"] = patients.with_these_clinical_events(
             codelist([code], system="snomed"),
-            between=["index_date", "index_date + 1 year"],
+            between=[start_date_formatted, end_date_formatted],
             returning="number_of_matches_in_period",
             return_expectations={
                 "incidence": 0.1,
@@ -23,6 +28,9 @@ def calculate_code_frequency(selected_codes):
     return variables
 
 
+start_date = datetime.strptime(study_start_date, "%Y-%m-%d").date()
+end_date = datetime.strptime(study_end_date, "%Y-%m-%d").date()
+
 selected_codelist = codelist_from_csv(
     codelist_file,
     system="snomed",
@@ -31,17 +39,16 @@ selected_codelist = codelist_from_csv(
 
 study = StudyDefinition(
     default_expectations={
-        "date": {"earliest": "index_date", "latest": "index_date + 1 year"},
+        "date": {"earliest": study_start_date, "latest": study_end_date},
         "rate": "uniform",
         "incidence": 0.5,
     },
-    index_date="1900-01-01",  # this will be replaced by what's specified in project.yaml
+    index_date=study_start_date,
     population=patients.satisfying(
         "currently_registered OR has_died",
-        currently_registered=patients.registered_as_of("index_date + 1 year"),
+        currently_registered=patients.registered_as_of(study_end_date),
         has_died=patients.with_death_recorded_in_primary_care(
-            between=["index_date", "index_date + 1 year"], returning="binary_flag"
+            between=[study_start_date, study_end_date], returning="binary_flag"
         ),
     ),
-    **calculate_code_frequency(selected_codelist),
-)
+    **calculate_code_frequency(start_date, end_date, selected_codelist),
