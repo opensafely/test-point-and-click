8c426f1949407dbf05d96fae3fc103eaa17770bb
HelenCEBM <helencebm@gmail.com>
2022-04-13 17:05:23 +0100

    Make variables consistent with project.yaml

--- analysis/variables.py
+++ analysis/variables.py
@@ -1,5 +1,5 @@
 codelist_file = "codelists/opensafely-systolic-blood-pressure-qof.csv"
 
-study_start_date = "2020-01-01"
-study_end_date = "2020-12-31"
+study_start_date = "2021-01-01"
+study_end_date = "2021-12-31"
 frequency = "monthly"
