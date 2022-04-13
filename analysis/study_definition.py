cc389f7fb02849fdcd713b5f56df517d1e5dfbf7
HelenCEBM <helencebm@gmail.com>
2022-04-13 14:46:24 +0100

    Use last_day_of_month

--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -18,21 +18,21 @@ selected_codelist = codelist_from_csv(
 
 study = StudyDefinition(
     default_expectations={
-        "date": {"earliest": "index_date", "latest": "index_date + 1 year"},
+        "date": {"earliest": "index_date", "latest": "last_day_of_month(index_date)"},
         "rate": "uniform",
         "incidence": 0.5,
     },
     index_date="1900-01-01",  # this will be replaced by what's specified in project.yaml
     population=patients.satisfying(
         "currently_registered OR has_died",
-        currently_registered=patients.registered_as_of("index_date + 1 year"),
+        currently_registered=patients.registered_as_of("index_date"),
         has_died=patients.with_death_recorded_in_primary_care(
-            between=["index_date", "index_date + 1 year"], returning="binary_flag"
+            between=["index_date", "last_day_of_month(index_date)"], returning="binary_flag"
         ),
     ),
     episodes=patients.with_these_clinical_events(
         codelist=selected_codelist,
-        between=["index_date", "index_date + 1 year"],
+        between=["index_date", "last_day_of_month(index_date)"],
         episode_defined_as="series of events each <= 0 days apart",
         returning="number_of_episodes",
         return_expectations={
