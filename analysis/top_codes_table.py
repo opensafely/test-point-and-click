211f7b813ca485e9a016430ee340f74df1416c9c
Louis Fisher <37254732+LFISHER7@users.noreply.github.com>
2022-04-14 10:15:15 +0100

    fix file extension

--- analysis/top_codes_table.py
+++ analysis/top_codes_table.py
@@ -10,7 +10,7 @@ OUTPUT_DIR = BASE_DIR / "output"
 codelist = pd.read_csv(codelist_file)
 
 def main():
-    df = pd.read_csv(OUTPUT_DIR / "input_counts.csv")
+    df = pd.read_csv(OUTPUT_DIR / "input_counts.csv.gz")
     top_5_code_table = create_top_5_code_table(df=df, code_df=codelist, code_column='code', term_column='term', low_count_threshold=low_count_threshold)
     top_5_code_table.to_csv(OUTPUT_DIR / 'top_5_code_table.csv', index=False)
 
