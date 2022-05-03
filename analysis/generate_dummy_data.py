8a8c31006ca16fffedb067a918022408697c52ca
Louis Fisher <37254732+LFISHER7@users.noreply.github.com>
2022-05-03 17:53:55 +0100

    add list size to dummy data

--- analysis/generate_dummy_data.py
+++ analysis/generate_dummy_data.py
@@ -2,6 +2,7 @@ import random
 from datetime import date, timedelta
 from pathlib import Path
 import pandas as pd
+import numpy as np
 
 random.seed(123)
 
@@ -9,6 +10,7 @@ random.seed(123)
 Path("output/dummy").mkdir(parents=True, exist_ok=True)
 
 rows = []
+rows_list_size = []
 
 for code in range(16):
     for date_offset in range(70):
@@ -26,6 +28,13 @@ for code in range(16):
                 ]
             )
 
+            rows_list_size.append(
+                [
+                    f"{practice:02}",
+                    int(random.gauss(2000, 500))
+                ]
+            )
+
 
 counts = pd.DataFrame(rows, columns=["code", "date", "practice", "num"])
 counts["date"] = pd.to_datetime(counts["date"])
@@ -36,3 +45,6 @@ counts_per_code.to_csv("output/dummy/counts_per_code.csv")
 grouper = pd.Grouper(key="date", freq="W-MON")
 counts_per_week = counts.groupby(["practice", grouper])["num"].sum()
 counts_per_week.to_csv("output/dummy/counts_per_week_practice.csv")
+
+list_size = pd.DataFrame(rows_list_size, columns=["practice", "list_size"])
+list_size.to_csv("output/dummy/list_sizes.csv")
