68d90341d20357a770a2fb53e77efb8a73d8b75b
Louis Fisher <37254732+LFISHER7@users.noreply.github.com>
2022-05-09 10:57:06 +0100

    use outputs from codelist report in top code table

--- analysis/top_codes_table.py
+++ analysis/top_codes_table.py
@@ -2,20 +2,26 @@ import pandas as pd
 from pathlib import Path
 from study_utils import create_top_5_code_table
 from variables import codelist_dict, low_count_threshold
-import glob 
+import glob
 import os
+
 BASE_DIR = Path(__file__).parents[1]
 OUTPUT_DIR = BASE_DIR / "output"
 
 
 def main():
-    for path in glob.glob(os.path.join(OUTPUT_DIR, "input_counts_*.feather")):
-        df = pd.read_feather(path)                                                                                                                                                                                                                                 
-        # fetch codelist used for measure
-        s = path[:path.index(".")].partition("_")[2].partition("_")[2]
-        codelist = pd.read_csv(codelist_dict[s])
-        top_5_code_table = create_top_5_code_table(df=df, code_df=codelist, code_column='code', term_column='term', low_count_threshold=low_count_threshold)
-        top_5_code_table.to_csv(OUTPUT_DIR / f'top_5_code_table_{s}.csv', index=False)
+    code_df = pd.read_csv(OUTPUT_DIR / "dummy/counts_per_code.csv")
+    codelist = pd.read_csv(codelist_dict["bp"])
+
+    top_5_code_table = create_top_5_code_table(
+        df=code_df,
+        code_df=codelist,
+        code_column="code",
+        term_column="term",
+        low_count_threshold=low_count_threshold,
+    )
+    top_5_code_table.to_csv(OUTPUT_DIR / f"top_5_code_table_{s}.csv", index=False)
+
 
 if __name__ == "__main__":
     main()
